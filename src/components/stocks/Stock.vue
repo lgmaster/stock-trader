deadc71b8bf1f44bd10c9304e26585bdf1fac28e
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-09-24 12:54:04 -0300

    feat: add stock options

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,10 +1,40 @@
 <template>
-  <h1>Ação</h1>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg3>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>{{ stock.name }} <small>(Preço: {{ stock.price }})</small></strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field label="Quantidade" type="number" v-model.number="quantity" />
+        <v-btn class="green darken-3 white--text" :disabled="quantity <= 0 || !Number.isInteger(quantity)" @click="buyStock">Comprar</v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
 export default {
-  name: "Stock"
+  name: "Stock",
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+
+      console.log(order)
+      this.quantity = 0
+    }
+  }
 }
 </script>
 
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,24 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+  </v-layout>
 </template>
 
 <script>
+import Stock from './Stock'
 export default {
-  name: "Stocks"
+  name: "Stocks",
+  components: { Stock },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 110 },
+        { id: 2, name: 'Google', price: 120 },
+        { id: 3, name: 'Apple', price: 130 },
+        { id: 4, name: 'Twitter', price: 140 },
+      ]
+    }
+  }
 }
 </script>
 
